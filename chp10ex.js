45d7f11e36734791e3ab21278ddf2c5037844f8d
Daniel Culpepper <culpepperd2490@gmail.com>
2023-08-27 12:21:22 -0400

    Chp 10, pg. 440 Sharpen Your Pencil

--- chp10ex.js
+++ chp10ex.js
@@ -2,6 +2,33 @@
     It's important to see functions as values, too.
 */
 
+// Chp 10 - pg. 440 "Sharpen Your Pencil"
+var winner = function() { alert("WINNER!") };
+var loser = function() { alert("LOSER!") };
+
+// test
+// winner();
+
+// assigning variables for practice
+var a = winner;
+var b = loser;
+var c = loser;
+
+a();
+b();
+
+// shell game
+c = a;
+a = b;
+b = c;
+c = a; 
+a = c;
+a = b;
+b = c;
+a();
+
+/* 
+// Chp 10 - pg. 439
 // quack() is defined with a function declaration
 function quack(num) {
     for (var i = 0; i < num; i++) {
@@ -21,3 +48,4 @@ superFly(2);
 
 var superQuack = quack;
 superQuack(3);
+*/
